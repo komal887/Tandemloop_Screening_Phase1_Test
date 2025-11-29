55bfba4cff540aa9fda90c1fd009466b7cea9c2c
Komal A <komalambekar91@gmail.com>
2025-11-29 06:57:45 +0530

    Enhance program-2.java with explanatory comments
    Added comments to clarify code functionality.

--- program-2.java
+++ program-2.java
@@ -1,12 +1,17 @@
-import java.util.*;
+import java.util.*;   // Importing the utility package to use the Scanner class
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int a =sc.nextInt();
 
-        for(int i = 1;i <= a;i++) {
-            System.out.print((2*i - 1) + " ");
+        Scanner sc = new Scanner(System.in); // Creating Scanner object to take user input
+        
+        int a = sc.nextInt();  // Reading an integer input from the user
+
+        // Loop runs from 1 to 'a' to generate first 'a' odd numbers
+        for (int i = 1; i <= a; i++) {
+
+            // Formula for the ith odd number: (2 * i - 1)
+            System.out.print((2 * i - 1) + " ");
         }
     }
 }
